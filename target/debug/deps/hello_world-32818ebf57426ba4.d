902bc42677974dd094e3b96950bc4f946b650779
sheMnapion <983579778@qq.com>
2019-01-07 14:32:16 +0800

    starting my first Rust project

---
+++ target/debug/deps/hello_world-32818ebf57426ba4.d
@@ -0,0 +1,5 @@
+/home/lac/RustPractices/target/debug/deps/hello_world-32818ebf57426ba4: src/main.rs
+
+/home/lac/RustPractices/target/debug/deps/hello_world-32818ebf57426ba4.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/lac/RustPractices/target/debug/hello_world: /home/lac/RustPractices/src/main.rs
