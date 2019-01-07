7608a9ac3fdf226aae9579490869c704e9de32aa
sheMnapion <983579778@qq.com>
2019-01-07 14:58:47 +0800

    finishing a basic guessing number project

---
+++ target/debug/deps/hello_world-866ee3b2a468b51e.d
@@ -0,0 +1,5 @@
+/home/lac/RustPractices/target/debug/deps/hello_world-866ee3b2a468b51e: src/main.rs
+
+/home/lac/RustPractices/target/debug/deps/hello_world-866ee3b2a468b51e.d: src/main.rs
+
+src/main.rs:
